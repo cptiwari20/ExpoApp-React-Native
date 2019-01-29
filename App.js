146535a79323cb8b218afed3507bf8c498387b78
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2019-01-29 16:21:20 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button, Header, Card, CardSection, Input } from './src/commons/index'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Header headerText='Expo App' />
+        <View style={styles.container}>
+          <Text>Great!</Text>
+        </View>
+        <Card>
+          <CardSection>
+            <Input label='Email' placeholder='example@mail.com'/>
+          </CardSection>
+          <CardSection>
+            <Input label='Password' placeholder='your secret password' />
+          </CardSection>
+          <CardSection>
+            <Button>
+              Login Now
+            </Button>
+          </CardSection>
+        </Card>
+      </View>
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
