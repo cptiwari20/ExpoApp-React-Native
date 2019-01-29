7e8a30047022afc1baf763ca354991f4b7ff1372
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2019-01-29 20:00:50 +0530

    added thunk to the store

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk'
 import reducers from './src/reducers'
 import LoginForm from './src/components/LoginForm';
 
 class App extends Component {
   render() {
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
         <View>
         <LoginForm /> 
         </View>
