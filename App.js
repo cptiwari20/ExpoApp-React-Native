932ae88625e29a4a762467144a3e79b0259fc2d4
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2019-01-29 17:47:52 +0530

    added store

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers'
 import LoginForm from './src/components/LoginForm';
 
 class App extends Component {
   render() {
     return (
-      <View>
-       <LoginForm /> 
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View>
+        <LoginForm /> 
+        </View>
+      </Provider>
     );
   }
 }
